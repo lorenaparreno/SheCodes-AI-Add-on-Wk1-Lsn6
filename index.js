cd8daac184c946b4a72f99ce681ff2c5535982d2
lorenaparreno <137244224+lorenaparreno@users.noreply.github.com>
2024-02-11 17:49:23 -0500

    Change getAttribute to setAttribute & bckgrdColor

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let disabledButtonElement = document.querySelector("#disabled-button");
-disabledButtonElement.getAttribute("disabled");
+disabledButtonElement.setAttribute("disabled", "disabled");
 
 let greenButtonElement = document.querySelector("#green-button");
-greenButtonElement.style.background = "green";
+greenButtonElement.style.backgroundColor = "green";
 
 let redButtonElement = document.querySelector("#red-button");
 redButtonElement.classList.add("danger");
