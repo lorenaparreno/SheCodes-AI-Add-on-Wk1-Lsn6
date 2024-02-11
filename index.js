8c0ef1338e40eaa52856fb44667c1fbc2542dc37
lorenaparreno <137244224+lorenaparreno@users.noreply.github.com>
2024-02-11 17:22:49 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let disabledButtonElement = document.querySelector("#disabled-button");
+disabledButtonElement.getAttribute("disabled");
+
+let greenButtonElement = document.querySelector("#green-button");
+greenButtonElement.style.background = "green";
+
+let redButtonElement = document.querySelector("#red-button");
+redButtonElement.classList.add("danger");
